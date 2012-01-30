2c6d80eab3b32a04d4abed3462c06bf6f7241734
Henrik Thostrup Jensen <htj@ndgf.org>
2012-01-30 09:32:26 +0000

    set version in bart 005 tag

--- bart/__init__.py
+++ bart/__init__.py
@@ -1,8 +1,5 @@
 # bart/__init__.py
 
-import time
-gmt = time.gmtime()
-
 # set this to "correct" version when making a release
-__version__ = 'svn-%04d%02d%02d' % (gmt.tm_year, gmt.tm_mon, gmt.tm_mday)
+__version__ = '005'
 
